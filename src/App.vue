56615441e74f77714eeef92ebff7da6bbc76faeb
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-10-18 10:19:18 +0700

    fix all

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <router-view v-slot="{Component}">
-    <transition name="fade" mode="out-in">      
-      <component :is="Component"/>
-    </transition>
-  </router-view>
+   <router-view v-slot="{ Component }">
+      <Transition name="fade" mode="out-in">
+         <component :is="Component" />
+      </Transition>
+   </router-view>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style type="text/css">
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s ease;
+   transition: opacity 0.5s ease;
 }
 .fade-enter-from,
 .fade-leave-to {
-  opacity: 0;
+   opacity: 0;
 }
-
 </style>
