67d17102587219f1b9c2ae4491e8f63223995790
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2023-01-01 18:23:39 +0700

    add belife

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -9,15 +9,17 @@
 
       <div class="container py-20">
          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
-            <div class="relative overflow-hidden group rounded-2xl" v-for="project in portfolio" :key="project.id">
-               <img class="object-cover w-full h-full group-hover:scale-110 group-hover:blur-sm transition-all duration-200 ease-out" :src="'/portfolio/' + project.img" :alt="'Image ' + project.title" />
-
-               <div class="p-4 md:p-8 flex flex-col justify-end bg-slate-900/40 inset-0 absolute opacity-0 scale-110 group-hover:opacity-100 group-hover:scale-100 ttransition-all duration-300 ease-out">
-                  <h2 class="text-lg md:text-2xl font-semibold text-white">{{ project.title }}</h2>
-                  <p class="text-slate-100 mt-2">{{ project.desc }}</p>
-                  <a class="mt-4 w-[130px] btn-primary" :href="project.url" target="_blank">See Project</a>
+            <div class="flex flex-col gap-6 group rounded-2xl p-8 bg-white" v-for="project in portfolio" :key="project.id">
+               <div class="relative overflow-hidden rounded-xl">
+                  <img class="object-cover w-full h-44 sm:h-full md:h-60 transition-all duration-200 ease-out" :src="'/portfolio/' + project.img" :alt="'Image ' + project.title" />
+                  <span class="absolute top-0 right-0 text-[12px] px-6 py-1 bg-primary rounded-bl-2xl text-white">{{ project.type }}</span>
                </div>
-               <span class="absolute top-0 right-0 text-sm px-6 py-1 text-white bg-primary rounded-bl-2xl">{{ project.type }}</span>
+
+               <div>
+                  <h2 class="text-lg md:text-xl font-semibold line-clamp-1" :title="project.title">{{ project.title }}</h2>
+                  <p class="text-slate-500 mt-4 text-sm md:text-base">{{ project.desc }}</p>
+                  <a class="mt-6 w-[130px] btn-primary" :href="project.url" target="_blank">See Project</a>
+               </div>
             </div>
          </div>
       </div>
